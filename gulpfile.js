a7c7c10145b432562171f89d06a06f61e841a327
PictureElement <marios.sofokleous@yandex.com>
2020-09-20 13:53:13 +0300

    Complete todo tasks.

--- gulpfile.js
+++ gulpfile.js
@@ -38,6 +38,7 @@ function copy() {
     'node_modules/@fortawesome/fontawesome-free/css/fontawesome.min.css',
     'node_modules/@fortawesome/fontawesome-free/css/brands.min.css',
     'node_modules/@fortawesome/fontawesome-free/css/solid.min.css',
+    'node_modules/@fortawesome/fontawesome-free/css/regular.min.css',
     'node_modules/@fortawesome/fontawesome-free/webfonts/**/*',
     'node_modules/aos/dist/aos.js',
     'node_modules/aos/dist/aos.css',
